feea03a297d42e278e3b13ff6c99f45537116ef4
David R. MacIver <david@drmaciver.com>
2015-05-24 16:04:39 +0100

    Make tests run in verbose mode

--- conchbackend_check.c
+++ conchbackend_check.c
@@ -30,7 +30,7 @@ int main(void) {
   s = conchbackend_suite();
   sr = srunner_create(s);
 
-  srunner_run_all(sr, CK_NORMAL);
+  srunner_run_all(sr, CK_VERBOSE);
   number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
   return (number_failed == 0) ? 0 : 1;
