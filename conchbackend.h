834ac25e584524d0993578ed41ff895ddab168de
David R. MacIver <david@drmaciver.com>
2015-05-24 15:14:51 +0100

    Revised API after discussion with cursing team

--- conchbackend.h
+++ conchbackend.h
@@ -2,6 +2,7 @@
 #define _CONCH_BACKEND_H
 
 #include <stdint.h>
+#include <libpq-fe.h>
 
 typedef uint64_t id;
 
@@ -19,21 +20,27 @@ typedef struct {
 typedef struct {
   // Invariant: If count > 0 then error == 0
   int error;
+  id before_token;
+  id after_token;
   uint64_t count;
   blast *blasts;
 } result_set;
-
-typedef struct {} mouthpiece;
 
 typedef struct {
   uint16_t page_size;
 } settings;
 
+typedef struct {
+  PGConn *connection;
+  settings settings; 
+} mouthpiece;
+
 mouthpiece *connect(settings settings);
 void disconnect(mouthpiece *mp);
 
 result_set *recent_blasts(mouthpiece *mp);
-result_set *blasts_before(mouthpiece *mp, id before_id);
+result_set *blasts_before(mouthpiece *mp, id before_token);
+result_set *blasts_after(mouthpiece *mp, id after_token);
 
 void free_result_set(result_set *results);
 
