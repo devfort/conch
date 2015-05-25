dc4810eac3803a82f130e5b2859c7a60839413e6
Alex Stapleton <alexs@prol.etari.at>
2015-05-25 16:48:31 +0100

    Remove duplicate init_color because I am bad at rebasing

--- conch.c
+++ conch.c
@@ -136,19 +136,6 @@ void render_conch(WINDOW *window) {
   delwin(cwin);
 }
 
-void init_colors() {
-  start_color();
-  use_default_colors();
-
-  init_color(100, 333, 333, 333);
-
-  init_pair(NORMAL_COLOR, COLOR_WHITE, COLOR_BLACK);
-  init_pair(NEW_COLOR, COLOR_BLUE, COLOR_BLUE);
-  init_pair(SELECTED_COLOR, COLOR_WHITE, COLOR_RED);
-  init_pair(TIMELINE_COLOR, 100, -1);
-  init_pair(STUCK_COLOR, COLOR_GREEN, COLOR_GREEN);
-}
-
 WINDOW *init_screen() {
   setlocale(LC_ALL, "");
   initscr();
