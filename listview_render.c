39bccd21badb9b2784749efe75205c1f90fcfe5f
Adam Sven Johnson <adam@pkqk.net>
2015-05-25 22:25:42 +0100

    Add an a emoji shell instead of an ascii one
    🐚 👍

--- listview_render.c
+++ listview_render.c
@@ -77,7 +77,7 @@ static void render_help(WINDOW *window) {
 
 static void render_chrome(WINDOW *window) {
   box(window, 0, 0);
-  mvwaddstr(window, 0, 3, " conch <@ ");
+  mvwaddstr(window, 0, 3, " conch 🐚  ");
   curs_set(0);
 
   if (MIN_WIDTH_FOR_CLOCK <= getmaxx(window)) {
