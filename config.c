bc9660dfff6e9fe28538ea4a6296db5211aa8b2d
Adam Sven Johnson <adam@pkqk.net>
2015-05-28 11:38:36 +0100

    Check config file exists and is readable

--- config.c
+++ config.c
@@ -1,4 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
@@ -8,11 +13,23 @@
 
 void parse_config(const char *filename, settings *settings) {
   lua_State *L = luaL_newstate();
-  luaL_dofile(L, filename);
+  if(luaL_dofile(L, filename)) {
+    fprintf(stderr, "Couldn't read config file %s\n", filename);
+  } else {
+    lua_getglobal(L, "username");
+    int idx = lua_gettop(L);
+    settings->username = strclone(lua_tostring(L, idx));
+  }
+}
 
-  lua_getglobal(L, "username");
-  int idx = lua_gettop(L);
-  settings->username = strclone(lua_tostring(L, idx));
+bool use_config(const char *filename) {
+  struct stat buf;
+  if(stat(filename, &buf) == 0) {
+    return true;
+  } else {
+    fprintf(stderr, "Couldn't read config file %s: %s\n", filename, strerror(errno));
+    return false;
+  }
 }
 
 settings conch_load_config(const char *filename) {
@@ -21,7 +38,9 @@ settings conch_load_config(const char *filename) {
   };
 
   char *config_path = expand_home(filename);
-  parse_config(config_path, &settings);
+  if (use_config(config_path)) {
+    parse_config(config_path, &settings);
+  }
   free(config_path);
 
   return settings;
