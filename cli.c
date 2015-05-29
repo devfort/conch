60c8be1e62f080a9ace6cea4c7b76e1d0d52a2ff
James Aylett <james@tartarus.org>
2015-05-29 17:57:03 +0100

    ./conch --help
    Also responds to -h and -?. This means that database host is now set
    using -H.

--- cli.c
+++ cli.c
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "config.h"
 #include "cli.h"
@@ -18,6 +19,23 @@ static void merge_config(conch_cli_options *options, settings *config) {
   }
 }
 
+void usage(char const *const progname) {
+  printf("Usage: %s [OPTIONS]\n", progname);
+  printf("ncurses client for bugle.\n");
+
+  printf("\nGeneral options:\n");
+  printf("  -h, --help\t\tdisplay this help and exit\n");
+  printf("  -s, --stick-to-top\tauto-display new blasts\n");
+  printf("  -c, --config=FILE\tuse FILE for configuration (default: %s)\n",
+         DEFAULT_CONFIG_LOCATION);
+  printf("\nOptions (these override the config file):\n");
+  printf("  -u, --username=USER\tuser to post as (register via bugle)\n");
+  printf("  -H, --host=HOSTNAME\tdatabase server host (default: %s)\n",
+         DEFAULT_DATABASE_HOST);
+  printf("  -d, --database=DBNAME\tdatabase name (default: %s)\n",
+         DEFAULT_DATABASE_NAME);
+}
+
 conch_cli_options conch_parse_command_line_args(int argc, char **argv) {
   conch_cli_options parsed_options = {
     .stick_to_head = false,
@@ -27,24 +45,28 @@ conch_cli_options conch_parse_command_line_args(int argc, char **argv) {
   int opt;
   char *config_filename = DEFAULT_CONFIG_LOCATION;
   static struct option longopts[] = {
+    { "help", no_argument, NULL, 'h' },
     { "stick-to-top", no_argument, NULL, 's' },
     { "config", required_argument, NULL, 'c' },
-    // TODO: -h should possibly be equivalent to --help (also make that)
-    { "host", required_argument, NULL, 'h' },
+    { "host", required_argument, NULL, 'H' },
     { "database", required_argument, NULL, 'd' },
     { "username", required_argument, NULL, 'u' },
     { NULL, 0, NULL, 0 },
   };
 
-  while ((opt = getopt_long(argc, argv, "sc:h:d:u:", longopts, NULL)) != -1) {
+  while ((opt = getopt_long(argc, argv, "h?sc:H:d:u:", longopts, NULL)) != -1) {
     switch (opt) {
+    case 'h':
+    case '?':
+      usage(argv[0]);
+      exit(0);
     case 'c':
       config_filename = optarg;
       break;
     case 's':
       parsed_options.stick_to_head = true;
       break;
-    case 'h':
+    case 'H':
       parsed_options.host = optarg;
       break;
     case 'd':
@@ -54,6 +76,7 @@ conch_cli_options conch_parse_command_line_args(int argc, char **argv) {
       parsed_options.username = optarg;
       break;
     default:
+      usage(argv[0]);
       exit(1);
     }
   }
