ed31d22d0f3ff70fc97a21698d6a4288171ec335
Ash Berlin <ash_github@firemirror.com>
2015-05-29 14:54:34 +0100

    Make the format to fatal_error a format specifier so we get compiler saftey
    Setting this magic attribute means if you don't pass enough arguments
    we'll get an error like this:
    
        webfetcher.c:54:63: error: more '%' conversions than data arguments [-Werror,-Wformat]
            fatal_error("conch_webfetcher_get: curl_easy_init failed %d!");
                                                                     ~^

--- explode.h
+++ explode.h
@@ -1,6 +1,13 @@
 #ifndef __EXPLODE_H
-#define  __EXPLODE_H
+#define __EXPLODE_H
 
-void fatal_error(const char *fmt, ...);
+#include <sys/cdefs.h>
+
+#ifndef __printflike
+#define __printflike(fmtarg, firstvararg)                                      \
+  __attribute__((__format__(__printf__, fmtarg, firstvararg)))
+#endif
+
+void fatal_error(const char *fmt, ...) __printflike(1, 2);
 
 #endif
