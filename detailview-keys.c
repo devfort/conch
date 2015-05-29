52fb8f51b483a06c7bff5582a228fa333026c615
Ash Berlin <ash_github@firemirror.com>
2015-05-29 10:32:49 +0100

    hjkl work in the detail view now

--- detailview-keys.c
+++ detailview-keys.c
@@ -7,15 +7,19 @@ extern void fatal_error(const char *, ...);
 keypress_result conch_detailview_keypress_dispatch(int key, detailview *v) {
 
   switch (key) {
+  case 'j':
   case KEY_DOWN:
     conch_detailview_scroll_down(v);
     break;
+  case 'k':
   case KEY_UP:
     conch_detailview_scroll_up(v);
     break;
+  case 'h':
   case KEY_LEFT:
     conch_detailview_scroll_code_left(v);
     break;
+  case 'l':
   case KEY_RIGHT:
     conch_detailview_scroll_code_right(v);
     break;
